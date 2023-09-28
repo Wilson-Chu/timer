ad5524d54ead762deb12e11488670666c515834b
Wilson Chu <wilson1.chu@gmail.com>
2023-09-27 20:45:09 -0400

    completed exercise

--- timer1.js
+++ timer1.js
@@ -3,9 +3,9 @@ const input = process.argv;
 const alarmTimes = input.slice(2);
 
 for (const alarm of alarmTimes) {
-  // if ((alarm * 1 !== alarm) || alarm < 0) {
-  //   continue;
-  // }
+  if ((isNaN(alarm)) || alarm < 0) {
+    continue;
+  }
 
   setTimeout(() => {
     // process.stdout.write('\x07'); // => Not working on my PC
